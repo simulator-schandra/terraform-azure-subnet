32f728d6b8f193c41db18979fc895813a37daac6
Suyash Chandra <suyash.chandra@fci-ccm.com>
2025-01-08 22:26:01 +0530

    suyash : wip

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
-module "private_subnet" {
-  source      = "./module"
-  subnet_name = ["simulator-sub-pvt-1", "simulator-sub-pvt-2"]
-  rg_name     = "simulator-rg"
-  vnet_name   = "simulator-vnet"
-  subnet_cidr = [["10.0.4.0/23"], ["10.0.6.0/23"]]
+resource "azurerm_subnet" "subnet" {
+  count = length(var.subnet_name)
+  name                                          = var.subnet_name[count.index]
+  resource_group_name                           = var.rg_name
+  virtual_network_name                          = var.vnet_name
+  address_prefixes                              = var.subnet_cidr[count.index]
+  default_outbound_access_enabled               = var.default_outbound_access_enabled
+  private_endpoint_network_policies             = var.private_endpoint_network_policies
+  private_link_service_network_policies_enabled = var.private_link_service_network_policies_enabled
 }
